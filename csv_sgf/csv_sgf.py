14d47e06da28e52695d52155bb2797bc03d27993
Moritama <moritama1515@gmail.com>
2016-09-19 00:40:04 +0900

    Alphabet changes to value

--- csv_sgf/csv_sgf.py
+++ csv_sgf/csv_sgf.py
@@ -7,16 +7,17 @@ import string
 f = open('001.txt','r')
 Allf = f.read()
 
-#a-zのリストを作成
-charList = [chr(k) for k in range(97,97+26)]
+#19路盤の座標(a-s)の検索文字列を作成
+char_list = [chr(k) for k in range(97,97+19)]
+search_list = ''.join(char_list)
 
 text = Allf.replace('\n','')
 text = text.replace('\r','')
 textList = text.split(";")
 
 #座標関連データ部分を取り出す
-play = textList[2:-2]
-
+play = textList[2:-1]
+print play
 #座標関連データをlistからstrに変換
 jw = "\n".join(play)
 
@@ -32,6 +33,8 @@ value =  maps.split("\n")
 #csv形式に変換(手数,x座標,y座標)
 for i,v in enumerate(value):
 	map_list = list(v)
+	map_list[0] = str(search_list.find(map_list[0])+1)
+	map_list[1] = str(search_list.find(map_list[1])+1)
 	map_list = ','.join(map_list)
 	print str((i+1)) + "," + map_list
 
