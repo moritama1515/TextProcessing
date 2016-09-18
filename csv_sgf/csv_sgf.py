d009d33dea1e079dc5ecd232e297c544c26a4bd3
Moritama <moritama1515@gmail.com>
2016-09-19 01:22:05 +0900

    add function and change README

--- csv_sgf/csv_sgf.py
+++ csv_sgf/csv_sgf.py
@@ -4,25 +4,27 @@ import sys
 import re
 import string
 
-f = open('001.txt','r')
-Allf = f.read()
+#コマンドラインくら引数を受け取りファイルを開く
+argvs = sys.argv
+fr = open(argvs[1],'r')
+Allfr = fr.read()
 
 #19路盤の座標(a-s)の検索文字列を作成
 char_list = [chr(k) for k in range(97,97+19)]
 search_list = ''.join(char_list)
 
-text = Allf.replace('\n','')
+text = Allfr.replace('\n','')
 text = text.replace('\r','')
-textList = text.split(";")
+text_list = text.split(";")
 
 #座標関連データ部分を取り出す
-play = textList[2:]
-print play
+play = text_list[2:]
+
 #座標関連データをlistからstrに変換
-jw = "\n".join(play)
+str_play = "\n".join(play)
 
 #座標関連データの座標部分のみを取り出す
-maps = jw.replace("Black",'')
+maps = str_play.replace("Black",'')
 maps = maps.replace("White",'')
 maps = maps.replace("[",'')
 maps = maps.replace("]",'')
@@ -31,12 +33,17 @@ maps = maps.replace(")",'')
 #listに格納
 value =  maps.split("\n")
 
+#書き込み用ファイルを開く
+wfile = argvs[1].split(".")
+fw = open(wfile[0]+".csv",'w') 
+
 #csv形式に変換(手数,x座標,y座標)
 for i,v in enumerate(value):
 	map_list = list(v)
 	map_list[0] = str(search_list.find(map_list[0])+1)
 	map_list[1] = str(search_list.find(map_list[1])+1)
 	map_list = ','.join(map_list)
-	print str((i+1)) + "," + map_list
-
-f.close()
+	fw.write(str((i+1)) + "," + map_list +"\n")
+	
+fw.close()
+fr.close()
